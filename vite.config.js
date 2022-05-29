888691facf5b4d2c5349351adc1adf739845cf89
sweetcter <daylasep0175@gmail.com>
2022-05-29 13:40:02 +0700

    d

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+const { resolve } = require('path')
+const { defineConfig } = require('vite')
+
+module.exports = defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        detail: resolve(__dirname, 'detail.html'),
+        products: resolve(__dirname, 'products.html'),
+
+       
+      }
+    }
+  }
+});
