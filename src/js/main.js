6d00eb35dda0ff1d43bca182e6c6bdda2fc32ed0
sweetcter <daylasep0175@gmail.com>
2022-05-29 13:37:27 +0700

    update

--- src/js/main.js
+++ src/js/main.js
@@ -2,5 +2,4 @@ import './style.css'
 
 document.querySelector('#app').innerHTML = `
   <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>`;
